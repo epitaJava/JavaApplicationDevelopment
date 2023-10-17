f856b131ba9b4d285fd481660c832be17d967bb2
Vaishnavi <147934171+epitaJava@users.noreply.github.com>
2023-10-17 15:54:07 +0200

    Update LoopExercise.java

--- LoopExercise.java
+++ LoopExercise.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class LoopExercise {
 	Scanner sc = new Scanner(System.in);
 	public void reverse() {
-		/*System.out.print("Enter the number:");
+		System.out.print("Enter the number:");
 		int num = sc.nextInt();
 		int rev= 0 , temp = num, rem = 0;
 		
@@ -19,17 +19,7 @@ public class LoopExercise {
 			rem = temp % 10;
 			rev = rev * 10 + rem;
 		}
-		System.out.println("Reversed number : " + rev);*/
-		String name = "vaishnavi";
-		int n = name.length();
-		for(int  i = 1 ; i <= n ; i ++) {
-			for(int space = n - i; space >= 1; space -- ) {
-				System.out.print("  ");
-			}
-			for(int j = 0 ; j < i ; j ++) {		
-				System.out.print("  "+name.charAt(j) + " ");
-			}
-			System.out.println();
-		}
-	}
+		System.out.println("Reversed number : " + rev);
+		
+}
 }
